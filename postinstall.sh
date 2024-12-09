96a972f86828cc01b3e35d45e0d7d74844a5a410
abhinavlg <71110461+abhinavlg@users.noreply.github.com>
2024-12-09 21:39:52 +0530

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-# Install the Playwright browser binaries
-python -m playwright install
+# Update package list
+sudo apt-get update
+
+# Install missing libraries
+sudo apt-get install -y \
+  libgstgl-1.0-0 \
+  libgstcodecparsers-1.0-0 \
+  libavif15 \
+  libenchant2-2 \
+  libsecret-1-0 \
+  libmanette-0.2-0 \
+  libgles2-mesa
